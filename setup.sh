6b6d49176a3d7673164c9af0ecfb5d36ac86ec1d
James Lee <batmanatucsd@gmail.com>
2015-01-19 03:02:09 -0800

    created setup.sh for creating symbolic links for .vimrc and .tmux.conf

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# vimrc setup
+echo "Setting up vimrc file: "
+if [ ! -f ~/.vimrc ]; then
+	ln -s ~/env/vim/.vimrc ~/.vimrc
+	echo "Created a symbolic link for .vimrc"
+else
+	echo " .vimrc already exists"
+	echo -n "Do you want to overwrite the file? (y/n): "
+	read input
+	if [ "$input" == y ]; then
+		ln -s ~/env/vim/.vimrc ~/.vimrc
+		echo "Created a symbolic link for .vimrc"
+	fi
+fi
+
+# tmux config file setup
+echo -e "\nSetting up tmux conf file: "
+if [ ! -f ~/.tmux.conf ]; then
+	ln -s ~/env/tmux/.tmux.conf ~/.tmux.conf
+	echo "Created a symbolic link for .tmux.conf"
+else
+	echo " .tmux.conf already exists"
+	echo -n "Do you want to overwrite the file? (y/n): "
+	read input
+	if [ "$input" == y ]; then
+		rm ~/.tmux.conf
+		ln -s ~/env/tmux/.tmux.conf ~/.tmux.conf
+		echo "Created a symbolic link for .tmux.conf"
+	fi
+fi
